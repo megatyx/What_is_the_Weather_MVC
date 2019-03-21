97ee1b674f246ee3dee9768fa7f417195529111e
Tyler Wells <twells@twells-mn1.local>
2019-03-21 18:35:25 +0800

    Collapsing and Expandable View

---
+++ What_is_the_Weather_Demo/What_is_the_Weather_Demo/View/Reusable/FlexibleWeatherView/FlexibleVerticalWeatherView.swift
@@ -0,0 +1,86 @@
+//
+//  FlexibleVerticalWeatherView.swift
+//  What_is_the_Weather_Demo
+//
+//  Created by Tyler Wells on 3/21/19.
+//  Copyright © 2019 Tyler Wells. All rights reserved.
+//
+
+import UIKit
+
+class FlexibleVerticalWeatherView: UIView {
+
+    @IBOutlet weak var degreesLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var weatherIconContainerView: UIView! {
+        didSet {
+             self.weatherIconContainerView.layer.cornerRadius = self.weatherIconContainerView.frame.size.width/2
+            self.weatherIconContainerView.clipsToBounds = true
+        }
+    }
+    @IBOutlet weak var weatherIconContainerViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var weatherIconContainerViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+
+
+
+    var weatherData: Forecast?
+
+    var shouldExpand: Bool? {
+        didSet {
+            guard let shouldExpand = shouldExpand else {return}
+            if shouldExpand {
+                self.expandView()
+            } else {
+                self.collapseView()
+            }
+        }
+    }
+
+    var shouldAnimateExpands: Bool = true
+    var shouldAnimateCollapses: Bool = true
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.collapseView()
+    }
+
+    private func expandView() {
+        func inLineExpand() {
+            self.tableView.isHidden = false
+            self.weatherIconContainerViewBottomConstraint.constant = 16
+        }
+        if shouldAnimateExpands {
+            UIView.animate(withDuration: 0.1, animations: {
+                inLineExpand()
+            })
+        } else {
+            inLineExpand()
+        }
+    }
+
+    private func collapseView() {
+        func inLineCollapse() {
+            self.tableView.isHidden = true
+            self.weatherIconContainerViewBottomConstraint.constant = self.frame.height / 2
+        }
+        if shouldAnimateExpands {
+            UIView.animate(withDuration: 0.1, animations: {
+                inLineCollapse()
+            })
+        } else {
+            inLineCollapse()
+        }
+    }
+
+}
+
+extension FlexibleVerticalWeatherView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
---
+++ What_is_the_Weather_Demo/What_is_the_Weather_Demo/ViewControllers/WeatherForcastViewController/WeatherForcastViewController.swift
@@ -0,0 +1,72 @@
+//
+//  WeatherForcastViewController.swift
+//  What_is_the_Weather_Demo
+//
+//  Created by Tyler Wells on 3/21/19.
+//  Copyright © 2019 Tyler Wells. All rights reserved.
+//
+
+import UIKit
+
+
+
+class WeatherForcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension WeatherForcastViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    // MARK: UICollectionViewDataSource
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+
+        // Configure the cell
+
+        return UICollectionViewCell()
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+
+     }
+     */
+
+}
